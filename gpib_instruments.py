7cc6882bf0c45aec475f700f6effabc9c1645b5f
QTG2 Laptop <helical email>
2023-01-25 09:45:31 -0600

    print option for no instrument

--- gpib_instruments.py
+++ gpib_instruments.py
@@ -3,10 +3,12 @@ import pyvisa
 
 rm = pyvisa.ResourceManager()
 addresses = rm.list_resources()
-
-print('Connected instruments (GPIB):')
-for address in addresses:
-    instrument = rm.open_resource(address)
-    instrument_id = instrument.query('*IDN?').rstrip()
-    print(f'  {address}\t({instrument_id})')
+if len(addresses) == 0:
+    print('No instruments detected')
+else:
+    print('Connected instruments (GPIB):')
+    for address in addresses:
+        instrument = rm.open_resource(address)
+        instrument_id = instrument.query('*IDN?').rstrip()
+        print(f'  {address}\t({instrument_id})')
 # print('\n'.join(instruments))
